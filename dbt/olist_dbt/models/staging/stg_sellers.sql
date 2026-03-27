a603b57a8c7f1b9645ca98e3d3db61f59279dcaf
Gideon Kipkorir <soigo@Gideons-MacBook-Pro.local>
2026-03-27 12:41:39 -0400

    fix(staging) - standardize column data types across staging models

--- dbt/olist_dbt/models/staging/stg_sellers.sql
+++ dbt/olist_dbt/models/staging/stg_sellers.sql
@@ -1,6 +1,6 @@
 SELECT 
     seller_id,
-    seller_zip_code_prefix,
+    NULLIF(seller_zip_code_prefix::text,'')::text AS seller_zip_code_prefix,
     seller_city,
     seller_state
 FROM {{source('raw', 'sellers')}}
