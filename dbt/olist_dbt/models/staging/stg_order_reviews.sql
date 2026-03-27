e5b760b496e2c6882c4260a9331f9de8cd4028b1
gideonlexy <gideonlexy@gmail.com>
2026-03-27 12:56:34 -0400

    Fix staging column types  (#7)
    * fix(staging) - standardize column data types across staging models
    
    * test(staging) - update constraints and composite keys tests
    
    * chore(dbt_utils)- add dbt_utils packages
    
    ---------
    
    Co-authored-by: Gideon Kipkorir <soigo@Gideons-MacBook-Pro.local>

--- dbt/olist_dbt/models/staging/stg_order_reviews.sql
+++ dbt/olist_dbt/models/staging/stg_order_reviews.sql
@@ -1,9 +1,9 @@
 SELECT
     review_id,
     order_id,
-    review_score,
+    review_score::int AS review_score,
     review_comment_title,
     review_comment_message,
-    review_creation_date,
-    review_answer_timestamp
+    NULLIF(review_creation_date, '')::date AS review_creation_date,
+    NULLIF(review_answer_timestamp, '')::timestamp AS review_answer_timestamp
 FROM {{source('raw', 'order_reviews')}}
--- dbt/olist_dbt/models/staging/stg_products.sql
+++ dbt/olist_dbt/models/staging/stg_products.sql
@@ -1,11 +1,11 @@
 SELECT
     product_id,
     product_category_name,
-    product_name_lenght AS product_name_length,
-    product_description_lenght AS product_description_length,
-    product_photos_qty,
-    product_weight_g,
-    product_length_cm, 
-    product_height_cm,
-    product_width_cm
+    product_name_lenght::int AS product_name_length,
+    product_description_lenght::int AS product_description_length,
+    product_photos_qty::int AS product_photos_qty,
+    product_weight_g::numeric(10,2) AS product_weight_g,
+    product_length_cm::numeric(10,2) AS product_length_cm, 
+    product_height_cm::numeric(10,2) AS product_height_cm,
+    product_width_cm::numeric(10,2) AS product_width_cm
 FROM {{source('raw', 'products')}}
