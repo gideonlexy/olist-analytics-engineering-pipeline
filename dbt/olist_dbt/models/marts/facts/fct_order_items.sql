5640db0b4c97db5c39414155e12b82c39dad409b
gideonlexy <gideonlexy@gmail.com>
2026-03-28 20:44:07 -0400

    Refine fct_order_items model and tests (#10)
    * fix(facts): cast calculated field type on fct_order_items
    
    * test(facts): add tests for fct_order_items
    
    * test(facts): correct fct_order_items grain description
    
    ---------
    
    Co-authored-by: Gideon Kipkorir <soigo@Gideons-MacBook-Pro.local>

--- dbt/olist_dbt/models/marts/facts/fct_order_items.sql
+++ dbt/olist_dbt/models/marts/facts/fct_order_items.sql
@@ -9,14 +9,18 @@ SELECT
     o.order_status,
     o.order_purchase_timestamp,
     o.order_approved_at,
+    o.order_delivered_carrier_date,
     o.order_delivered_customer_date,
     o.order_estimated_delivery_date,
 
     oi.shipping_limit_date,
 
-    oi.price,
-    oi.freight_value,
-    (oi.price + oi.freight_value) AS gross_item_value
+    oi.price AS item_price ,
+    oi.freight_value AS freight_value,
+    (oi.price + oi.freight_value)::NUMERIC(12,2) AS gross_item_value,
+
+    1 AS item_quantity
+    
 FROM {{ ref('stg_order_items') }} oi
 JOIN {{ ref('stg_orders') }} o
     ON oi.order_id = o.order_id
