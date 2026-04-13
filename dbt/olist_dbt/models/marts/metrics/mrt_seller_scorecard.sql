8dd13d1c61261d817e2096fe8fb34f8806a28903
gideonlexy <gideonlexy@gmail.com>
2026-04-13 17:19:25 -0400

    fix(mart): update mrt_seller_scorecard with correct on_time_orders and on_time_rate logic (#27)
    Co-authored-by: Gideon Kipkorir <soigo@Gideons-MacBook-Pro.local>

--- dbt/olist_dbt/models/marts/metrics/mrt_seller_scorecard.sql
+++ dbt/olist_dbt/models/marts/metrics/mrt_seller_scorecard.sql
@@ -5,10 +5,16 @@ SELECT
     SUM(seller_order_items) AS total_items_sold,
     SUM(seller_order_revenue) AS total_revenue,
     COUNT(DISTINCT CASE WHEN is_delivered = TRUE THEN order_id END) AS delivered_orders,
-    COUNT(DISTINCT CASE WHEN is_on_time = TRUE THEN order_id END) AS on_time_orders,
-    COUNT(DISTINCT CASE WHEN is_late = TRUE THEN order_id END) AS late_orders,
+    COUNT(
+        DISTINCT CASE WHEN is_delivered = TRUE AND is_on_time = TRUE THEN order_id END
+    ) AS on_time_orders,
+    COUNT(
+        DISTINCT CASE WHEN is_delivered = TRUE AND is_late = TRUE THEN order_id END
+    ) AS late_orders,
     ROUND(
-        COUNT(DISTINCT CASE WHEN is_on_time = TRUE THEN order_id END)::NUMERIC
+        COUNT(
+            DISTINCT CASE WHEN is_delivered = TRUE AND is_on_time = TRUE THEN order_id END
+        )::NUMERIC
         / NULLIF(COUNT(DISTINCT CASE WHEN is_delivered = TRUE THEN order_id END), 0),
         4
     ) AS on_time_rate
