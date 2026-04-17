59d0f8f400bc3db5a45cdcc0bdeca9276c0b15ef
gideonlexy <gideonlexy@gmail.com>
2026-04-17 11:41:05 -0400

    fix(marts): add estimated_delivery_duration_days field to fct_delivery and compututed it's avg on mrt_delivery_sla (#31)
    Co-authored-by: Gideon Kipkorir <soigo@Gideons-MacBook-Pro.local>

--- dbt/olist_dbt/models/marts/metrics/mrt_delivery_sla.sql
+++ dbt/olist_dbt/models/marts/metrics/mrt_delivery_sla.sql
@@ -21,7 +21,8 @@ SELECT
         CASE
             WHEN is_late = TRUE THEN delivery_delay_days
         END
-    ) AS avg_late_delay_days
+    ) AS avg_late_delay_days,
+    AVG(estimated_delivery_duration_days) AS avg_estimated_delivery_duration_days
 
 FROM {{ ref('fct_delivery') }}
 WHERE
